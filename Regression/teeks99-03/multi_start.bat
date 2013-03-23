6dccea0993d20f092e21b2a9193fecea10474fee
Tom Kent <teeks99@yahoo.com>
2013-03-22 20:17:48 -0500

    cleanup temp

--- Regression/teeks99-03/multi_start.bat
+++ Regression/teeks99-03/multi_start.bat
@@ -6,6 +6,7 @@ call vars.bat
 python run.py --runner=teeks99-03%id%-win7-64on64 --force-update --toolsets=%toolset% --bjam-options="-j4 address-model=64" --comment=..\info.html 2>&1 | wtee output.log
 
 rd /s/q results
+rd /s/q %TEMP%
 
 del last_run.log
 move run_started.log last_run.log

bd5173882d486553cc56a9cc244860b4b0ff0006
Tom Kent <tomkent@lisa.(none)>
2013-04-22 16:48:52 -0500

    clean dirs

---
+++ Regression/teeks99-03/cleanup.bat
@@ -0,0 +1,34 @@
+
+cd a
+rd /s/q results
+cd ..
+
+cd b
+rd /s/q results
+cd ..
+
+cd c
+rd /s/q results
+cd ..
+
+cd d
+rd /s/q results
+cd ..
+
+cd e
+rd /s/q results
+cd ..
+
+cd f
+rd /s/q results
+cd ..
+
+cd g
+rd /s/q results
+cd ..
+
+cd h
+rd /s/q results
+cd ..
+
+

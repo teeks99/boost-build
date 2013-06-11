bf6ca372dd75168784410503a77a4cb782f1bef3
Tom Kent <teeks99@yahoo.com>
2013-06-11 09:56:43 -0500

    tabs

--- Regression/multi_run.py
+++ Regression/multi_run.py
@@ -30,8 +30,8 @@ class Runner(object):
             run['compilers'], '--bjam-options="-j' + str(self.mvs['procs']) + 
             ' address-model=' + run['arch'] + '"', '--comment=..\info.html']
 
-	if run['type'] == 'release':
-		command.append('--tag=branches/release')
+        if run['type'] == 'release':
+            command.append('--tag=branches/release')
 
         # Output the command to the screen before running it            
         cmd_str = ""

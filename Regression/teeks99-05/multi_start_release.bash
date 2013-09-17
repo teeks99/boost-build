aa4106a5a190c0a127cac686f7de4854589a556b
Tom Kent <teeks99@yahoo.com>
2013-09-17 15:22:47 -0500

    Changed number to 05

--- Regression/teeks99-05/multi_start_release.bash
+++ Regression/teeks99-05/multi_start_release.bash
@@ -4,7 +4,7 @@ echo Starting at: `date` > run_started.log
 rsync -a ../boost-release/ boost/
 source vars.bash
 
-python run.py --runner=teeks99-09${id}-Ubuntu12.04-64 --toolsets=${tools} --force-update --tag=branches/release --bjam-options=-j2 --comment=../info.html 2>&1 | tee output.log
+python run.py --runner=teeks99-05${id}-Ubuntu12.04-64 --toolsets=${tools} --force-update --tag=branches/release --bjam-options=-j2 --comment=../info.html 2>&1 | tee output.log
 
 rm -rf boost/ results/
 rm -rf /tmp/*.cpp

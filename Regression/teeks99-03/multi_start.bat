47036795017921a1f729d3863e3c8f8c0c0d3449
Tom Kent <teeks99@yahoo.com>
2013-10-25 07:47:21 -0500

    Cleanup invalid directory change

--- Regression/teeks99-03/multi_start.bat
+++ Regression/teeks99-03/multi_start.bat
@@ -1,10 +1,10 @@
-cd C:\local\regression
 echo Starting at: %DATE% %TIME% > run_started.log
 
 call vars.bat
 
 python run.py --timeout=10 --runner=teeks99-03%id%-win2008-64on64 --force-update --toolsets=%toolset% --bjam-options="-j2 address-model=64" --comment=..\info.html 2>&1 | ..\wtee output.log
 
+copy results\bjam.log .\
 rd /s/q results
 rd /s/q %TEMP%
 mkdir %TEMP%
--- Regression/teeks99-03/multi_start_release.bat
+++ Regression/teeks99-03/multi_start_release.bat
@@ -1,10 +1,10 @@
-cd C:\local\regression
 echo Starting at: %DATE% %TIME% > run_started.log
 
 call vars.bat
 
 python run.py --runner=teeks99-03%id%-win2008-64on64 --force-update --toolsets=%toolset% --tag=branches/release --bjam-options="-j2 address-model=64" --comment=..\info.html --timeout=10 2>&1 | ..\wtee output.log
 
+copy results\bjam.log .\
 rd /s/q results
 rd /s/q %TEMP%
 mkdir %TEMP%

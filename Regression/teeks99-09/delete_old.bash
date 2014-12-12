449895c6d0d8106b688e9eea5349b93d516a829a
Thomas Kent <teeks99@yahoo.com>
2014-12-12 07:07:38 -0600

    Added arm runner

---
+++ Regression/teeks99-09/delete_old.bash
@@ -0,0 +1,7 @@
+for ver in a b
+do
+   cd $ver
+   rm -rf boost_root
+   rm -rf results
+   cd ..
+done
---
+++ Regression/teeks99-09/multi_start.bash
@@ -0,0 +1,17 @@
+rm output.log
+echo Starting at: `date` > run_started.log
+
+rsync -a ../boost_root/ boost_root/
+pushd boost_root
+git checkout develop
+popd
+
+source vars.bash
+
+python run.py --runner=teeks99-09${id}-Ubuntu14.04-arm --toolsets=${tools} --force-update --bjam-options=-j4 --tag=develop --comment=../info.html 2>&1 | tee output.log
+
+rm -rf boost_root/ results/
+rm -rf tmp/*.cpp
+
+mv run_started.log last_run.log
+echo Finished at: `date` >> last_run.log

bea903596838f7292a4da9df3605a467f67e7723
Thomas Kent <teeks99@yahoo.com>
2015-07-14 06:59:02 -0500

    Added additional clang versions

--- Docker/build_images.bash
+++ Docker/build_images.bash
@@ -1,5 +1,23 @@
+
+pushd gcc-4.8
+docker build -t teeks99/gcc-4.8 .
+popd
+pushd gcc-4.9
+docker build -t teeks99/gcc-4.9 .
+popd
 pushd gcc-5.1
 docker build -t teeks99/gcc-5.1 .
+popd
+
+
+pushd clang-3.3
+docker build -t teeks99/clang-3.3 .
+popd
+pushd clang-3.4
+docker build -t teeks99/clang-3.4 .
+popd
+pushd clang-3.5
+docker build -t teeks99/clang-3.5 .
 popd
 pushd clang-3.6
 docker build -t teeks99/clang-3.6 .

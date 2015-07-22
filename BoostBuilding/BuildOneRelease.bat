ae6bd3e0a815e5244a06f27736dddd7c9a8a729a
Thomas Kent <teeks99@yahoo.com>
2015-07-21 20:55:02 -0500

    Added VC14 and VC12 update 5

--- BoostBuilding/BuildOneRelease.bat
+++ BoostBuilding/BuildOneRelease.bat
@@ -48,11 +48,13 @@ call:BuildVersion 9.0 32
 call:BuildVersion 10.0 32
 call:BuildVersion 11.0 32
 call:BuildVersion 12.0 32
+call:BuildVersion 14.0 32
 call:BuildVersion 8.0 64
 call:BuildVersion 9.0 64
 call:BuildVersion 10.0 64
 call:BuildVersion 11.0 64
 call:BuildVersion 12.0 64
+call:BuildVersion 14.0 64
 
 cd ..
 
@@ -78,11 +80,13 @@ call:MakeInstaller 9.0 32
 call:MakeInstaller 10.0 32
 call:MakeInstaller 11.0 32
 call:MakeInstaller 12.0 32
+call:MakeInstaller 14.0 32
 call:MakeInstaller 8.0 64
 call:MakeInstaller 9.0 64
 call:MakeInstaller 10.0 64
 call:MakeInstaller 11.0 64
 call:MakeInstaller 12.0 64
+call:MakeInstaller 14.0 64
 
 rd /S/Q %boost_version%
 move %boost_version%_complete %boost_version%

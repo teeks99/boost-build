e0ab97d17e1a0e4e97b48af9260d76e22502b61f
Thomas Kent <teeks99@yahoo.com>
2015-07-25 23:37:47 -0500

    Additional line in build log for command

--- ReleaseBuild/BuildOneRelease.bat
+++ ReleaseBuild/BuildOneRelease.bat
@@ -104,6 +104,7 @@ REM move stage\lib\* lib32-msvc-8.0\
 
 REM Build again to log any errors in the build process
 echo Build for msvc-%~1 >> %~2bitlog.txt
+echo %bjam% --without-mpi --build-type=complete toolset=msvc-%~1 address-model=%~2 architecture=x86 --prefix=.\ --libdir=lib%~2-msvc-%~1 --includedir=garbage_headers install >> %~2bitlog.txt
 %bjam% --without-mpi --build-type=complete toolset=msvc-%~1 address-model=%~2 architecture=x86 --prefix=.\ --libdir=lib%~2-msvc-%~1 --includedir=garbage_headers install >> %~2bitlog.txt 2<&1
 
 copy ..\DEPENDENCY_VERSIONS.txt lib%~2-msvc-%~1\

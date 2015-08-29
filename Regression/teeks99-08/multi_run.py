06295a800a1fb6306e45e36a28fcf300905050ee
Thomas Kent <teeks99@yahoo.com>
2015-08-28 20:48:07 -0500

    Ready to move to single

--- Regression/teeks99-08/multi_run.py
+++ Regression/teeks99-08/multi_run.py
@@ -1,4 +1,5 @@
 import tool_versions
+import single
 
 import shutil
 import os
@@ -34,74 +35,70 @@ def win_rmtree(directory):
     if os.path.isdir(directory):
         os.system('rmdir /S /Q \"{}\"'.format(directory))
 
+def my_rmtree(directory):
+    if os.path.exists(directory):
+        if os.name == 'nt':
+            win_rmtree(directory)
+        else:
+            shutil.rmtree(directory)
+
 class Runner(object):
     def __init__(self, machine_vars, cleanup=False):
-        self.mvs = machine_vars
-        self.runs = self.mvs['runs']
-        if 'run_order' in self.mvs:
-            self.run_order = self.mvs['run_order']
+        self.machine = machine_vars
+        self.runs = self.machine['runs']
+        if 'run_order' in self.machine:
+            self.run_order = self.machine['run_order']
         else:
             self.run_order = sorted(self.runs.keys())
         self.cleanup = False
         self.start_dir = os.getcwd()
-        self.current_run = None
-        self.sys_tmpdir = tempfile.gettempdir()
-
-    @property
-    def multi_run_log(self):
-        log_dir = os.path.join(self.start_dir, 'logs')
-        if not os.path.exists(log_dir):
-            os.mkdir(log_dir)
-        return os.path.join(log_dir, 'all_runs.log')
+        self.current_run_id = None
 
     def copy_repo(self, origin="../boost_root"):
         repo_name = "boost_root"
         print("copying repo " + origin + " to " + repo_name)
-        #shutil.rmtree(repo_name)
-        win_rmtree(repo_name)
         shutil.copytree(origin, repo_name)
 
-    def update_base_repo(self, branch):
-        orig_dir = os.getcwd()
-        try:
-            print('cd boost_root')
-            os.chdir('boost_root')
-
-            print('git checkout' + branch)
-            os.system('git checkout ' + branch)
-            print('git pull')
-            os.system('git pull')
-            print('git submodule init')
-            os.system('git submodule init')
-            print('git submodule update')
-            os.system('git submodule update')
-
-        finally:
-            os.chdir(orig_dir)
-
     def clean_and_make_tmp(self):
-        self.tmpdir = self.sys_tmpdir
-        if 'tmpdir' in self.mvs:
-            self.tmpdir = self.mvs['tmpdir']
+        tmpdir = tempfile.gettempdir()
+        if 'tmpdir' in self.machine:
+            tmpdir = self.machine['tmpdir']
         else:
-            self.tmpdir = os.path.join(self.tmpdir, "boost_regression")
+            tmpdir = os.path.join(tmpdir, "boost_regression")
 
-        if os.path.exists(self.tmpdir):
-            if os.name == 'nt':
-                win_rmtree(self.tmpdir)
-            else:
-                shutil.rmtree(self.tmpdir)
-        os.makedirs(self.tmpdir)
+        my_rmtree(tmpdir)
+        os.makedirs(tmpdir)
 
-        os.environ['TMPDIR'] = self.tmpdir
-        os.environ['TMP'] = self.tmpdir
-        os.environ['TEMP'] = self.tmpdir
+        os.environ['TMPDIR'] = tmpdir
+        os.environ['TMP'] = tmpdir
+        os.environ['TEMP'] = tmpdir
 
-    def branch(self):
-        return self.runs[self.current_run]['branch']
+    def make_info(self):
+        info_template = None
+        with open('../info.html.template', 'r') as info_template_file:
+            info_template = string.Template(info_template_file.read())
 
-    def run_one(self, run):
-        os.chdir(run['run_dir'])
+        mapping = {
+            'machine': self.machine['machine'],
+            'runner': self.config['id'],
+            'setup': self.machine['setup'],
+            'ram': self.machine['ram'],
+            'cores': self.machine['procs'],
+            'arch': self.machine['os_arch'],
+            'os': self.machine['os'],
+            'user_config': tool_versions.user_config(),
+            'site_config': tool_versions.site_config(),
+            'compiler_versions': tool_versions.build_version_string(),
+            'python_version': tool_versions.python_version(),
+            'git_version': tool_versions.git_version()}
+
+        info_str = info_template.substitute(mapping)
+
+        with open('info.html', 'w') as info_file:
+            info_file.write(info_str)
+
+    def run_one(self):
+        os.chdir(self.config['run_dir'])
 
         self.clean_and_make_tmp()
         self.make_info()
@@ -110,16 +107,16 @@ class Runner(object):
         shutil.copy2('../run.py', './')
 
         other_options = ''
-        if 'other_options' in run:
-            other_options = ' ' + run['other_options']
+        if 'other_options' in self.config:
+            other_options = ' ' + self.config['other_options']
 
-        command = ['python', 'run.py', '--runner=' + self.mvs['machine'] +
-            run['id'] + '-' + self.mvs['os'] + '-' + run['arch'] + "on" +
-            self.mvs['os_arch'], '--toolsets=' +
-            run['compilers'], '--bjam-options=-j' + str(self.mvs['procs']) +
-            ' address-model=' + run['arch'] + ' --abbreviate-paths' +
-            ' --remove-test-targets' + other_options, '--comment=info.html',
-            '--tag=' + self.branch()]
+        command = ['python', 'run.py', '--runner=' + self.machine['machine'] +
+            self.config['id'] + '-' + self.machine['os'] + '-' +
+            self.config['arch'] + "on" + self.machine['os_arch'], '--toolsets=' +
+            self.config['compilers'], '--bjam-options=-j' +
+            str(self.machine['procs']) + ' address-model=' + self.config['arch'] +
+            ' --abbreviate-paths' + ' --remove-test-targets' + other_options,
+            '--comment=info.html', '--tag=' + self.config['branch']]
 
         # Output the command to the screen before running it            
         cmd_str = ""
@@ -131,11 +128,13 @@ class Runner(object):
         print('at: ' + datetime.datetime.utcnow().isoformat(' ') + ' UTC')
         print('')
 
-        with open('../logs/' + run['id'] + '-output.log', 'w') as log_file:
+        with open('../logs/' + self.config['id'] + '-output.log', 'w') as \
+                log_file:
             log_file.write('Running command:\n:')
             log_file.write(cmd_str[1:])
             log_file.write('\n')
-            log_file.write('at: ' + datetime.datetime.utcnow().isoformat(' ') + ' UTC')
+            log_file.write(
+                'at: ' + datetime.datetime.utcnow().isoformat(' ') + ' UTC')
             log_file.write('\n\n\n')
 
             proc = subprocess.Popen(command,
@@ -150,18 +149,9 @@ class Runner(object):
             stderrThread.join()
 
         if os.path.isfile('results/bjam.log'):
-            shutil.copy2('results/bjam.log', '../logs/' + run['id'] +
+            shutil.copy2('results/bjam.log', '../logs/' + self.config['id'] +
                          '-results-bjam.log')
 
-        if self.cleanup:
-            try:
-                win_rmtree('results')
-                win_rmtree('boost_root')
-                #rmtree on temp???
-            except OSError:
-                pass # dir wasn't there...may indicate previous failure
-
-        self.log_end()
         os.chdir(self.start_dir)
 
     def loop(self, start_at=0):
@@ -177,13 +167,13 @@ class Runner(object):
             if order_index >= len(self.run_order):
                 order_index = 0
 
-            self.current_run = self.run_order[order_index]
+            self.current_run_id = self.run_order[order_index]
             run_dir = 'run'
 
             self.start_time = datetime.datetime.now()
             start_str = self.start_time.strftime('%m-%d %H:%M:%S')
 
-            run_config = self.runs[self.current_run].copy()
+            run_config = self.runs[self.current_run_id].copy()
             run_config['order_index'] = order_index
             run_config['start_time'] = start_str
             run_config['run_dir'] = run_dir
@@ -194,7 +184,11 @@ class Runner(object):
 
             self.write_run_config(run_config)
             self.log_start(run_config)
-            self.run_one(run_config)
+            self.config = run_config
+            self.run_one()
+            #run = single.Run(config=run_config, machine=self.machine)
+            #run.process()
+            self.log_end()
             order_index += 1
 
     def check_for_stop(self):
@@ -217,6 +211,13 @@ class Runner(object):
         with open(self.multi_run_log, "a") as log:
             log.write("\nStarting Runs at: " +
                 datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "\n")
+
+    @property
+    def multi_run_log(self):
+        log_dir = os.path.join(self.start_dir, 'logs')
+        if not os.path.exists(log_dir):
+            os.mkdir(log_dir)
+        return os.path.join(log_dir, 'all_runs.log')
 
     def log_start(self, config):
         print('')
@@ -242,29 +243,23 @@ class Runner(object):
                 datetime.datetime.now().strftime('%m-%d %H:%M:%S') + "in: " +
                 duration_hrs_str + "hrs\n")
 
-    def make_info(self):
-        info_template = None
-        with open('../info.html.template', 'r') as info_template_file:
-            info_template = string.Template(info_template_file.read())
+    def update_base_repo(self, branch):
+        orig_dir = os.getcwd()
+        try:
+            print('cd boost_root')
+            os.chdir('boost_root')
 
-        mapping = {
-            'machine': self.mvs['machine'],
-            'runner': self.current_run,
-            'setup': self.mvs['setup'],
-            'ram': self.mvs['ram'],
-            'cores': self.mvs['procs'],
-            'arch': self.mvs['os_arch'],
-            'os': self.mvs['os'],
-            'user_config': tool_versions.user_config(),
-            'site_config': tool_versions.site_config(),
-            'compiler_versions': tool_versions.build_version_string(),
-            'python_version': tool_versions.python_version(),
-            'git_version': tool_versions.git_version()}
+            print('git checkout ' + branch)
+            os.system('git checkout ' + branch)
+            print('git pull')
+            os.system('git pull')
+            print('git submodule init')
+            os.system('git submodule init')
+            print('git submodule update')
+            os.system('git submodule update')
 
-        info_str = info_template.substitute(mapping)
-
-        with open('info.html', 'w') as info_file:
-            info_file.write(info_str)
+        finally:
+            os.chdir(orig_dir)
 
 
 if __name__ == '__main__':
@@ -274,8 +269,8 @@ if __name__ == '__main__':
 
     r = Runner(machine_vars)
     if len(sys.argv) > 1:
-        r.current_run = sys.argv[1]
-        r.run_one(r.runs[r.current_run])
+        r.current_run_id = sys.argv[1]
+        r.run_one(r.runs[r.current_run_id])
     else:
         r.cleanup = True
         r.restart()

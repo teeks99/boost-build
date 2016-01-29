42b7103c67e777aedd45d6e4a727a4360ea32ff5
Tom Kent <tomkent@lisa>
2016-01-28 20:05:44 -0600

    Added docker images

--- Docker/build_images.bash
+++ Docker/build_images.bash
@@ -1,33 +1,9 @@
 
-pushd gcc-4.4
-docker build -t teeks99/gcc-4.4 .
-popd
-pushd gcc-4.6
-docker build -t teeks99/gcc-4.6 .
-popd
-pushd gcc-4.8
-docker build -t teeks99/gcc-4.8 .
-popd
-pushd gcc-4.9
-docker build -t teeks99/gcc-4.9 .
-popd
-pushd gcc-5.1
-docker build -t teeks99/gcc-5.1 .
-popd
+for compiler in gcc-4.4 gcc-4.6 gcc-4.8 gcc-4.9 gcc-5 clang-3.0 clang-3.3 clang-3.4 clang-3.5 clang-3.6 clang-3.7
+do
+  pushd $compiler
+  echo docker build -t teeks99/$compiler .
+  docker build -t teeks99/$compiler .
+  popd
+done
 
-
-pushd clang-3.0
-docker build -t teeks99/clang-3.0 .
-popd
-pushd clang-3.3
-docker build -t teeks99/clang-3.3 .
-popd
-pushd clang-3.4
-docker build -t teeks99/clang-3.4 .
-popd
-pushd clang-3.5
-docker build -t teeks99/clang-3.5 .
-popd
-pushd clang-3.6
-docker build -t teeks99/clang-3.6 .
-popd

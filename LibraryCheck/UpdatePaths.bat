b72e657e742b9633174addb2a34e678d55148b37
Thomas Kent <teeks99@yahoo.com>
2017-03-21 16:25:42 -0500

    Use 14.1 dirs for lib test

--- LibraryCheck/UpdatePaths.bat
+++ LibraryCheck/UpdatePaths.bat
@@ -33,8 +33,8 @@ bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-14.0/g" BoostLib
 bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-14.0/g" BoostLibraryCheck-VC14.vcxproj
 
 bin-sed\sed.exe -i "s/FILL_INC_PATH/%BOOST_PATH%/g" BoostLibraryCheck-VC14.1.vcxproj
-bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-14.10/g" BoostLibraryCheck-VC14.1.vcxproj
-bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-14.10/g" BoostLibraryCheck-VC14.1.vcxproj
+bin-sed\sed.exe -i "s/FILL_32_LINK_PATH/%BOOST_LIB32_PATH%-msvc-14.1/g" BoostLibraryCheck-VC14.1.vcxproj
+bin-sed\sed.exe -i "s/FILL_64_LINK_PATH/%BOOST_LIB64_PATH%-msvc-14.1/g" BoostLibraryCheck-VC14.1.vcxproj
 
 REM Cleanup garbage leftover by sed
 del sed*

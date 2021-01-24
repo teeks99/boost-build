73205fb83b379b7bedbc2ca37f67f4ee652c7835
Thomas Kent <tom@teeks99.com>
2021-01-24 14:32:14 -0600

    All msvc docker images

--- Regression/teeks99-11/run.py
+++ Regression/teeks99-11/run.py
@@ -15,9 +15,44 @@ import urllib
 ### ----------------------------- Unique --------------------------------------
 # Needed for running on GCC build farm
 import ssl
-tls_context = ssl.SSLContext(ssl.PROTOCOL_TLS)
-tls_context.load_verify_locations(cafile='/etc/ssl/certs/ca-certificates.crt')
 
+# Patch SSL to load correct cafile
+debian_cafile='/etc/ssl/certs/ca-certificates.crt'
+
+def patch_create_default_context(purpose=Purpose.SERVER_AUTH,
+                           cafile=debian_cafile,
+                           capath=None, cadata=None):
+    """Create a SSLContext object with default settings.
+    NOTE: The protocol and settings may change anytime without prior
+          deprecation. The values represent a fair balance between maximum
+          compatibility and security.
+    """
+    if not isinstance(purpose, _ASN1Object):
+        raise TypeError(purpose)
+
+    # SSLContext sets OP_NO_SSLv2, OP_NO_SSLv3, OP_NO_COMPRESSION,
+    # OP_CIPHER_SERVER_PREFERENCE, OP_SINGLE_DH_USE and OP_SINGLE_ECDH_USE
+    # by default.
+    context = SSLContext(PROTOCOL_TLS)
+
+    if purpose == Purpose.SERVER_AUTH:
+        # verify certs and host name in client mode
+        context.verify_mode = CERT_REQUIRED
+        context.check_hostname = True
+    elif purpose == Purpose.CLIENT_AUTH:
+        context.set_ciphers(_RESTRICTED_SERVER_CIPHERS)
+
+    if cafile or capath or cadata:
+        context.load_verify_locations(cafile, capath, cadata)
+    elif context.verify_mode != CERT_NONE:
+        # no explicit cafile, capath or cadata but the verify mode is
+        # CERT_OPTIONAL or CERT_REQUIRED. Let's try to load default system
+        # root CA certificates for the given purpose. This may fail silently.
+        context.load_default_certs(purpose)
+    return context
+
+keep_ssl_create_default_context = ssl.create_default_context
+ssl.create_default_context = patch_create_default_context
 ### -----------------------------End Unique -----------------------------------
 
 #~ Using --skip-script-download is useful to avoid repeated downloading of
